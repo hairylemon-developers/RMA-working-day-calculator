38e1d0d6ec64da6c06cc0a59e69e5a65125daa6f
Adrian Lim <adrian@hairylemon.co.nz>
2026-05-26 08:47:13 +1200

    back link to main services page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import ConsentCalculator from './components/ConsentCalculator'
 function App() {
   return (
     <div className="min-h-screen p-4 bg-gray-100">
+      <a href="https://www.inovo.nz/services" className="mx-auto block text-center text-green-500 hover:text-green-600 py-4">Back to Inovo Services Page</a>
       <ConsentCalculator />
+            <a href="https://www.inovo.nz/services" className="mx-auto block text-center text-green-500 hover:text-green-600 py-4">Back to Inovo Services Page</a>
     </div>
   )
 }
